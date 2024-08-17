375891dafc8946978944a7ba80c97017172455eb
Jonathanpv99 <jonyvegapjv@gmail.com>
2024-08-17 14:45:48 -0600

    feat: add react native papper && config it

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import {
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
 import Inicio from './views/Inicio';
 import NuevoCliente from './views/NuevoCliente';
 import DetallesCliente from './views/DetallesCliente';
@@ -17,11 +18,29 @@ import DetallesCliente from './views/DetallesCliente';
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
+
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+    }
+  };
  
   return (
     <NavigationContainer>
+       <StatusBar barStyle="light-content" backgroundColor="#2D4B73" />
       <Stack.Navigator
         initialRouteName='Inicio'
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: theme.colors.primary,
+          },
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: theme.colors.surface,
+          },
+          headerTitleAlign: 'center',
+        }}
       >
         <Stack.Screen
           name='Inicio'
@@ -36,7 +55,7 @@ function App(): React.JSX.Element {
         />
         <Stack.Screen
           name='DetallesCliente'
-          component={Inicio}
+          component={DetallesCliente}
           options={{
             title: 'Detalles Cliente',
           }}
