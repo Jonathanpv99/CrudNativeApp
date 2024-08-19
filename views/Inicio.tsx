44af5a688faf18da48ac0c7dbd584358e2770e36
Jonathanpv99 <jonyvegapjv@gmail.com>
2024-08-19 09:46:42 -0600

    fix: get clientes when Inicio is visible

--- views/Inicio.tsx
+++ views/Inicio.tsx
@@ -1,38 +1,73 @@
 import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
-  Text,
   View,
+  FlatList,
 } from 'react-native';
+import { 
+  List,
+  Headline,
+  FAB,
+} from 'react-native-paper';
 import axios from 'axios';
 import urlApi from '../api/url';
+import globalStyles from '../styles/global';
 
-function Inicio(): React.JSX.Element {
+function Inicio({
+  navigation,
+}): React.JSX.Element {
  
   const [clientes, setClientes] = useState([]);
-  
-  useEffect( () => {
-    const getClientesApi = async () => {
-      try {
-        const res = await axios.get(urlApi);
-        setClientes(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
 
-    getClientesApi();
-  },[]);
+  useEffect( () => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      const getClientesApi = async () => {
+        try {
+          const res = await axios.get(urlApi);
+          setClientes(res.data);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      getClientesApi();
+  });
+  return unsubscribe;
+  },[navigation]);
 
   return (
-    <View>
-      <Text>Inicio</Text>
+    <View style={globalStyles.contenedor}>
+      <Headline 
+        style={globalStyles.titulo}
+      >
+        {clientes.length > 0 ? 'Clientes' : 'Aún no hay clientes'}
+      </Headline>
+      <FlatList
+        data={clientes}
+        keyExtractor={ (cliente) => (cliente.id).toString() }
+        renderItem={ ({item}) => (
+          <List.Item
+            title={item.nombre}
+            description={item.empresa}
+          />
+        )}
+      />
+      <FAB
+        icon='plus'
+        style={styles.fab}
+        onPress={ () => navigation.navigate('NuevoCliente')}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  fab: {
+    position: 'absolute',
+    margin: 20,
+    right: 0,
+    bottom: 20,
+  },
 });
 
 export default Inicio;
